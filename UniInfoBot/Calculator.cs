b4caa1c084d002e2d9a1ffc8a7f736f7897fab9e
kuretchi <kuretchi@outlook.jp>
2017-08-15 15:29:50 +0900

    Initial Commit

---
+++ UniInfoBot/Calculator.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UniInfoBot
+{
+    public sealed class AcceptanceForSSS
+    {
+        public int AcceptableJustice { get; }
+
+        public int AcceptableAttack { get; }
+
+        public AcceptanceForSSS(int acceptableJustice, int acceptableAttack)
+        {
+            this.AcceptableJustice = acceptableJustice;
+            this.AcceptableAttack = acceptableAttack;
+        }
+    }
+
+    public sealed class AcceptanceFor9900
+    {
+        public int AcceptableJustice { get; }
+
+        public AcceptanceFor9900(int acceptableJustice)
+        {
+            this.AcceptableJustice = acceptableJustice;
+        }
+    }
+
+    public sealed class CalculationResult
+    {
+        public double Level { get; }
+
+        public int Notes { get; }
+
+        public IEnumerable<AcceptanceForSSS> AcceptancesForSSS { get; }
+
+        public AcceptanceFor9900 AcceptanceFor9900 { get; }
+
+        public CalculationResult(double level, int notes, IEnumerable<AcceptanceForSSS> acceptancesForSSS, AcceptanceFor9900 acceptanceFor9900)
+        {
+            this.Level = level;
+            this.Notes = notes;
+            this.AcceptancesForSSS = acceptancesForSSS;
+            this.AcceptanceFor9900 = acceptanceFor9900;
+        }
+    }
+
+    public static class Calculator
+    {
+        public static CalculationResult Calculate(double level, int notes)
+        {
+            const double justiceRate = 0.04;
+
+            const double scoreMax = 1010000;
+            const double score9900 = 1009900;
+            const double scoreSSS = 1007500;
+
+            var scorePerJusticeCritical = scoreMax / notes;
+            var scorePerJustice = scoreMax / notes * (100.0 / 101.0);
+            var scorePerAttack = scoreMax / notes * (50.0 / 101.0);
+
+            var justiceCountForSSS = new int?[] { null, (int)(notes * justiceRate), null };
+            var attackCountForSSS = new[] { 0, 0, 0 };
+
+            for (var i = 0; ; i++)
+            {
+                var score = (int)(scorePerJusticeCritical * (notes - justiceCountForSSS[1] - i)
+                    + scorePerJustice * justiceCountForSSS[1]
+                    + scorePerAttack * i);
+
+                if (score >= scoreSSS)
+                {
+                    attackCountForSSS[1] = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            attackCountForSSS[0] = attackCountForSSS[1] + 1;
+            attackCountForSSS[2] = attackCountForSSS[1] - 1;
+
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; ; j++)
+                {
+                    var score = (int)(scorePerJusticeCritical * (notes - attackCountForSSS[i] - j)
+                        + scorePerJustice * j
+                        + scorePerAttack * attackCountForSSS[i]);
+
+                    if (score >= scoreSSS)
+                    {
+                        justiceCountForSSS[i] = j;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            var justiceCountFor9900 = 0;
+
+            for (var i = 0; ; i++)
+            {
+                var score = (int)(scorePerJusticeCritical * (notes - i) + scorePerJustice * i);
+
+                if (score >= score9900)
+                {
+                    justiceCountFor9900 = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new CalculationResult(level, notes,
+                justiceCountForSSS
+                    .Zip(attackCountForSSS, (x, y) => (x, y))
+                    .Where(x => x.Item1.HasValue)
+                    .Select(x => new AcceptanceForSSS(x.Item1.Value, x.Item2)),
+                new AcceptanceFor9900(justiceCountFor9900));
+        }
+    }
+}
---
+++ UniInfoBot/MusicDataManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Timers;
+using Codeplex.Data;
+
+namespace UniInfoBot
+{
+    public sealed class Music
+    {
+        public int MusicId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Level { get; set; }
+
+        public int Notes { get; set; }
+    }
+
+    public sealed class MusicDataContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+
+        public MusicDataContext() : base(nameof(MusicDataContext)) { }
+    }
+
+    public sealed class NotFoundException : Exception
+    {
+        public override string Message => "曲が見つかりませんでした。";
+    }
+
+    public sealed class MusicDataManager
+    {
+        private HttpClient _client;
+        
+        private static readonly string _requestUri = "http://chuniviewer.net/GetMusicData.php";
+
+        private static readonly TimeSpan _updateSpan = TimeSpan.FromMinutes(10);
+
+        private Timer _timer;
+
+        private object _locker;
+
+        public MusicDataManager()
+        {
+            _client = new HttpClient();
+            _timer = new Timer(_updateSpan.TotalMilliseconds);
+            _timer.Elapsed += async (sender, e) => await UpdateDatabase();
+            _locker = new object();
+
+            ResetDatabase();
+            UpdateDatabase().Wait();
+
+            _timer.Start();
+        }
+
+        private async Task<dynamic> DownloadJsonData()
+        {
+            var response = await _client.PostAsync(_requestUri, null);
+            var json = await response.Content.ReadAsStringAsync();
+
+            const int maxLength = 10000;
+
+            for (var i = 0; i < maxLength; i++)
+            {
+                json = json.Replace($"\"{i}\"", $"\"_{i}\"");
+            }
+
+            return DynamicJson.Parse(json);
+        }
+
+        private async Task UpdateDatabase()
+        {
+            var json = await DownloadJsonData();
+
+            lock (_locker)
+            {
+                using (var context = new MusicDataContext())
+                {
+                    const int maxLength = 10000;
+
+                    for (var i = 0; i < maxLength; i++)
+                    {
+                        if (!json.IsDefined($"_{i}"))
+                        {
+                            continue;
+                        }
+
+                        Music data;
+                        var name = (string)json[$"_{i}"]["name"];
+
+                        if (json[$"_{i}"]["difficulty"].IsDefined($"_{3}"))
+                        {
+                            var level = (double)json[$"_{i}"]["difficulty"][$"_{3}"]["pattern_constant"];
+                            var notes = (int)Convert.ToInt32(json[$"_{i}"]["difficulty"][$"_{3}"]["notes"]);
+                            data = new Music() { Name = name, Level = level, Notes = notes };
+                        }
+                        else if (json[$"_{i}"]["difficulty"].IsDefined(3))
+                        {
+                            var level = (double)json[$"_{i}"]["difficulty"][3]["pattern_constant"];
+                            var notes = (int)Convert.ToInt32(json[$"_{i}"]["difficulty"][3]["notes"]);
+                            data = new Music() { Name = name, Level = level, Notes = notes };
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                        var music = context.Musics.SingleOrDefault(x => x.MusicId == data.MusicId);
+
+                        if (music == default(Music))
+                        {
+                            context.Musics.Add(data);
+                        }
+                        else
+                        {
+                            music = data;
+                        }
+
+                        context.SaveChanges();
+                    }
+                }
+            }
+        }
+
+        private void ResetDatabase()
+        {
+            lock (_locker)
+            {
+                using (var context = new MusicDataContext())
+                {
+                    context.Musics.RemoveRange(context.Musics);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public Music GetMusicData(string musicKeyword)
+        {
+            lock (_locker)
+            {
+                using (var context = new MusicDataContext())
+                {
+                    var music = context.Musics.ToArray().FirstOrDefault(x
+                        => x.Name.IndexOf(musicKeyword, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                    if (music == default(Music))
+                    {
+                        throw new NotFoundException();
+                    }
+
+                    return music;
+                }
+            }
+        }
+    }
+}
