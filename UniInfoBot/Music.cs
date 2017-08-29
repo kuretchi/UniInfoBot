0bde50c5a73a02594378186d614cb253312e0bc4
kuretchi <kuretchi@outlook.jp>
2017-08-30 00:24:02 +0900

    Change methods of getting/holding music data

---
+++ UniInfoBot/Music.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace UniInfoBot
+{
+    public enum Difficluty
+    {
+        Easy, Advanced, Expert, Master
+    }
+
+    public sealed class Music
+    {
+        public string Name { get; }
+
+        public IEnumerable<string> Keywords { get; }
+
+        public IReadOnlyDictionary<Difficluty, string> Level { get; }
+
+        public IReadOnlyDictionary<Difficluty, double?> Constant { get; }
+
+        public IReadOnlyDictionary<Difficluty, int?> Notes { get; }
+
+        public Music(dynamic json)
+        {
+            this.Name = json.Name;
+            this.Keywords = json.Keywords;
+
+            this.Level = new Dictionary<Difficluty, string>
+            {
+                { Difficluty.Easy, json.Level.Easy },
+                { Difficluty.Advanced, json.Level.Advanced },
+                { Difficluty.Expert, json.Level.Expert },
+                { Difficluty.Master, json.Level.Master },
+            };
+
+            this.Constant = new Dictionary<Difficluty, double?>
+            {
+                { Difficluty.Easy, json.Constant.Easy },
+                { Difficluty.Advanced, json.Constant.Advanced },
+                { Difficluty.Expert, json.Constant.Expert },
+                { Difficluty.Master, json.Constant.Master },
+            };
+
+            this.Notes = new Dictionary<Difficluty, int?>
+            {
+                { Difficluty.Easy, json.Notes.Easy },
+                { Difficluty.Advanced, json.Notes.Advanced },
+                { Difficluty.Expert, json.Notes.Expert },
+                { Difficluty.Master, json.Notes.Master },
+            };
+        }
+    }
+}
--- UniInfoBot/MusicDataManager.cs
+++ UniInfoBot/MusicDataManager.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Data.Entity;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -8,24 +8,6 @@ using Codeplex.Data;
 
 namespace UniInfoBot
 {
-    public sealed class Music
-    {
-        public int MusicId { get; set; }
-
-        public string Name { get; set; }
-
-        public double Level { get; set; }
-
-        public int Notes { get; set; }
-    }
-
-    public sealed class MusicDataContext : DbContext
-    {
-        public DbSet<Music> Musics { get; set; }
-
-        public MusicDataContext() : base(nameof(MusicDataContext)) { }
-    }
-
     public sealed class NotFoundException : Exception
     {
         public override string Message => "曲が見つかりませんでした。";
@@ -35,9 +17,12 @@ namespace UniInfoBot
     {
         private HttpClient _client;
         
-        private static readonly string _requestUri = "http://chuniviewer.net/GetMusicData.php";
+        private static readonly string _requestUri
+            = "https://raw.githubusercontent.com/kuretchi/chunithm-music/master/music.json";
 
         private static readonly TimeSpan _updateSpan = TimeSpan.FromMinutes(10);
+
+        private List<Music> _musicData;
 
         private Timer _timer;
 
@@ -48,111 +33,41 @@ namespace UniInfoBot
             _client = new HttpClient();
             _client.DefaultRequestHeaders.Add("User-Agent", "UniInfoBot (https://twitter.com/uni_info_bot)");
             _timer = new Timer(_updateSpan.TotalMilliseconds);
-            _timer.Elapsed += async (sender, e) => await UpdateDatabase();
+            _timer.Elapsed += async (sender, e) => await UpdateMusicData();
             _locker = new object();
 
-            ResetDatabase();
-            UpdateDatabase().Wait();
+            UpdateMusicData().Wait();
 
             _timer.Start();
-        }
-
-        private async Task<dynamic> DownloadJsonData()
-        {
-            var response = await _client.PostAsync(_requestUri, null);
-            var json = await response.Content.ReadAsStringAsync();
-
-            const int maxLength = 10000;
-
-            for (var i = 0; i < maxLength; i++)
-            {
-                json = json.Replace($"\"{i}\"", $"\"_{i}\"");
-            }
-
-            return DynamicJson.Parse(json);
-        }
-
-        private async Task UpdateDatabase()
-        {
-            var json = await DownloadJsonData();
-
-            lock (_locker)
-            {
-                using (var context = new MusicDataContext())
-                {
-                    const int maxLength = 10000;
-
-                    for (var i = 0; i < maxLength; i++)
-                    {
-                        if (!json.IsDefined($"_{i}"))
-                        {
-                            continue;
-                        }
-
-                        Music data;
-                        var name = (string)json[$"_{i}"]["name"];
-
-                        if (json[$"_{i}"]["difficulty"].IsDefined($"_{3}"))
-                        {
-                            var level = (double)json[$"_{i}"]["difficulty"][$"_{3}"]["pattern_constant"];
-                            var notes = (int)Convert.ToInt32(json[$"_{i}"]["difficulty"][$"_{3}"]["notes"]);
-                            data = new Music() { Name = name, Level = level, Notes = notes };
-                        }
-                        else if (json[$"_{i}"]["difficulty"].IsDefined(3))
-                        {
-                            var level = (double)json[$"_{i}"]["difficulty"][3]["pattern_constant"];
-                            var notes = (int)Convert.ToInt32(json[$"_{i}"]["difficulty"][3]["notes"]);
-                            data = new Music() { Name = name, Level = level, Notes = notes };
-                        }
-                        else
-                        {
-                            continue;
-                        }
-
-                        var music = context.Musics.SingleOrDefault(x => x.Name == data.Name);
-
-                        if (music == default(Music))
-                        {
-                            context.Musics.Add(data);
-                        }
-                        else
-                        {
-                            music = data;
-                        }
-
-                        context.SaveChanges();
-                    }
-                }
-            }
-        }
-
-        private void ResetDatabase()
-        {
-            lock (_locker)
-            {
-                using (var context = new MusicDataContext())
-                {
-                    context.Musics.RemoveRange(context.Musics);
-                    context.SaveChanges();
-                }
-            }
         }
 
         public Music GetMusicData(string musicKeyword)
         {
             lock (_locker)
             {
-                using (var context = new MusicDataContext())
+                var music = _musicData.FirstOrDefault(x
+                    => x.Name.IndexOf(musicKeyword, StringComparison.OrdinalIgnoreCase) >= 0);
+
+                if (music == default(Music))
                 {
-                    var music = context.Musics.ToArray().FirstOrDefault(x
-                        => x.Name.IndexOf(musicKeyword, StringComparison.OrdinalIgnoreCase) >= 0);
+                    throw new NotFoundException();
+                }
 
-                    if (music == default(Music))
-                    {
-                        throw new NotFoundException();
-                    }
+                return music;
+            }
+        }
 
-                    return music;
+        private async Task UpdateMusicData()
+        {
+            var json = DynamicJson.Parse(await _client.GetStringAsync(_requestUri));
+
+            lock (_locker)
+            {
+                _musicData.Clear();
+
+                for (var i = 0; json.IsDefined(i); i++)
+                {
+                    _musicData.Add(new Music(json[i]));
                 }
             }
         }
